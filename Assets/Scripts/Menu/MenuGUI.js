fdf54cbc16f9f11eb39466b235aa55c5fbb1d1ba
unknown <c7153961@JG103-13.aet.leedsbeckett.ac.uk>
2015-01-18 10:16:03 +0000

    added Menu gui

---
+++ Assets/Scripts/Menu/MenuGUI.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+
+class MenuGUI extends MonoBehaviour 
+{
+
+	public var hostBtnStyle : GUIStyle;
+	public var joinBtnStyle : GUIStyle;
+
+	public function OnGUI () : void 
+	{
+		if ( GUI.Button(Rect(100,100,200,50), "", this.hostBtnStyle))
+		{
+			
+		}
+		
+		if ( GUI.Button(Rect(100,200,200,50), "", this.joinBtnStyle))
+		{
+			
+			
+		}	
+	}
+}
